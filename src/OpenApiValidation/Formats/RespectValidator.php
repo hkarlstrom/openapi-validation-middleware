b092916bd2cc161e7d55386671338a64c1061b17
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2018-09-26 19:53:30 +0300

    RespectValidation fix

--- src/OpenApiValidation/Formats/RespectValidator.php
+++ src/OpenApiValidation/Formats/RespectValidator.php
@@ -35,7 +35,11 @@ class RespectValidator implements IFormat
 
     public function validate($data) : bool
     {
-        $v = v::{$this->validator}(...$this->args);
+        if (count($this->args)) {
+            $v = v::{$this->validator}(...$this->args);
+        } else {
+            $v = v::{$this->validator}();
+        }
         return $v->validate($data);
     }
 
@@ -46,9 +50,11 @@ class RespectValidator implements IFormat
         preg_match_all($pattern, $format, $matches);
         $validator = lcfirst(str_replace(' ', '', ucwords(str_replace('-', ' ', $matches['function'][0] ?? ''))));
         $args      = [];
-
         foreach (explode(',', $matches['arguments'][0]) as $arg) {
             $arg = trim($arg, "'");
+            if (!mb_strlen($arg)) {
+                continue;
+            }
             if ('true' == $arg) {
                 $arg = true;
             } elseif ('false' == $arg) {
