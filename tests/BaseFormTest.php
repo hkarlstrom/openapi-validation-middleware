80987a942a09d297ac136358776a9a5fb59871fa
Łukasz Rutkowski <lukasz.rutkowski@tauceti.email>
2019-12-01 16:46:55 +0000

    test proving that form data for content type 'application/x-www-form-urlencoded' is not validated - third part (detailed verification)

--- tests/BaseFormTest.php
+++ tests/BaseFormTest.php
@@ -41,30 +41,130 @@ class BaseFormTest extends TestCase
         $this->assertTrue($json['ok']);
     }
 
-    public function testFormDataErrors()
+    public function providerFormDataErrors()
+    {
+        return [
+            // id: invalid type (expected: integer)
+            'invalid_type' => [
+                [
+                    'id' => 'invalid-type',
+                    'text' => 'somestring',
+                    'array' => [
+                        ['name' => 'test', 'value' => 'test2'],
+                        ['name' => 'test', 'value' => 'test2'],
+                    ],
+                    'object' => [
+                        'name' => 'test',
+                        'value' => 'test2',
+                    ]
+                ],
+                [
+                    'message' => 'Request validation failed',
+                    'errors' => [
+                        [
+                            'name' => 'id',
+                            'code' => 'error_type',
+                            'value' => 'invalid-type',
+                            'in' => 'form-data',
+                            'expected' => 'integer',
+                            'used' => 'string',
+                        ],
+                    ],
+                ],
+            ],
+            // text: missing
+            'text_missing' => [
+                [
+                    'id' => 100,
+                    'array' => [
+                        ['name' => 'test', 'value' => 'test2'],
+                        ['name' => 'test', 'value' => 'test2'],
+                    ],
+                    'object' => [
+                        'name' => 'test',
+                        'value' => 'test2',
+                    ]
+                ],
+                [
+                    'message' => 'Request validation failed',
+                    'errors' => [
+                        [
+                            'name' => 'text',
+                            'code' => 'error_required',
+                            'in' => 'form-data',
+                        ],
+                    ],
+                ],
+            ],
+            // object: missing value property
+            'object_no_property' => [
+                [
+                    'id' => 100,
+                    'text' => 'somestring',
+                    'array' => [
+                        ['name' => 'test', 'value' => 'test2'],
+                        ['name' => 'test', 'value' => 'test2'],
+                    ],
+                    'object' => [
+                        'name' => 'test',
+                    ]
+                ],
+                [
+                    'message' => 'Request validation failed',
+                    'errors' => [
+                        [
+                            'name' => 'object.value',
+                            'code' => 'error_required',
+                            'in' => 'form-data',
+                        ],
+                    ],
+                ],
+            ],
+            // array: not matching minItems validation
+            'array_to_few_items' => [
+                [
+                    'id' => 100,
+                    'text' => 'somestring',
+                    'array' => [
+                        ['name' => 'test', 'value' => 'test2'],
+                    ],
+                    'object' => [
+                        'name' => 'test',
+                        'value' => 'test2',
+                    ]
+                ],
+                [
+                    'message' => 'Request validation failed',
+                    'errors' => [
+                        [
+                            'name' => 'array',
+                            'code' => 'error_minItems',
+                            'value' => [
+                                ['name' => 'test', 'value' => 'test2'],
+                            ],
+                            'in' => 'form-data',
+                            'min' => 2,
+                            'count' => 1,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerFormDataErrors
+     */
+    public function testFormDataErrors(array $formData, array $expectedResponse)
     {
         $response = $this->response('post', '/form-data', [
-            'formData' => [
-                'id' => 'invalid-type',
-                'text' => 'somestring',
-                'arr' => [
-                    ['name' => 'test', 'value' => 'test2'],
-                ],
-                'object' => [
-                    'name' => 'test',
-                    'value' => 'test2',
-                ]
-            ],
+            'formData' => $formData,
         ]);
-        // id: invalid type (expected: integer)
-        // text: missing
-        // object: missing value property
-        // array: not matching minItems validation
 
         $json = $this->json($response);
 
-        // TODO: Detailed response validation
         $this->assertSame(400, $response->getStatusCode());
+        $this->assertSame($expectedResponse, $json);
     }
 
     protected function json(ResponseInterface $response) : array
